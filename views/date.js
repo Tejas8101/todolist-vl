4fdc0ee8ed382c2a656b51894394da843f7523a6
Tejas6969 <tejas.shinde8101@gmail.com>
2022-08-03 10:22:25 +0530

    Todo List project

---
+++ views/date.js
@@ -0,0 +1,17 @@
+
+const { syncBuiltinESMExports } = require('node:module');
+module.exports = getDate;
+
+function getDate(){
+  let today = new Date();
+  let currentDay = today.getDay();
+
+  let options={
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+  return day;
+}
